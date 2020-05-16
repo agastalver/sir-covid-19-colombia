ebf3a9ca4b3cd17144e4b88bb0be4f9f66263498
Adolfo Gastalver Rubio <agastalver@ingelectus.com>
2020-05-16 09:48:33 +0200

    updating 2020-05-15

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ def fdelay_lockdown(delay, lckday, nlckdays):
     return result
 
 lckday = dft.index.get_loc(pd.to_datetime("2020-03-25"))
-nlckdays = 46 # days
+nlckdays = 61 # days
 
 delay = 0 # days back
 result = fdelay_lockdown(delay, lckday, nlckdays)
